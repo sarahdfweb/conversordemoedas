758030e07eb3a2cd080f84ffb1f980ed985aedaf
Sarah Silva <87348787+sarahdfweb@users.noreply.github.com>
2022-09-07 16:34:07 -0300

    Add files via upload
    Conversor de moedas, projeto feito na imersão dev alura.

---
+++ main.js
@@ -0,0 +1,12 @@
+function Converter() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor);
+  
+    var valorEmReal = valorEmDolarNumerico * 5.25;
+    console.log(valorEmReal);
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "A conversão  será de R$" + valorEmReal;
+    elementoValorConvertido.innerHTML = valorConvertido,valor,valorEmReal;
+  }
+  
